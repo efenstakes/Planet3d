5c31bb088182f4027aa54263743e1069e13c81ab
felix-muoki <c-fmuoki@thearenagroup.net>
2022-07-16 09:59:21 +0300

    add input action function

--- src/utils/input.utils.ts
+++ src/utils/input.utils.ts
@@ -1,6 +1,8 @@
 
 
 
+export enum GameInput { JUMP, FORWARD, REVERSE, RIGHT, LEFT }
+
 export enum UserInput { Up, Right, Down, Left }
 
 
@@ -26,6 +28,41 @@ export const getInput = (event: KeyboardEvent): UserInput | null => {
         case 'ArrowDown':
             userInput = UserInput.Down
             break;
+                    
+        default:
+            userInput = null
+            break;
+    }
+
+    return userInput;
+}
+
+
+export const getBallGameInput = (event: KeyboardEvent): GameInput | null => {
+    let userInput: GameInput | null
+
+    switch (event.code) {
+        case 'KEYA':
+            userInput = GameInput.RIGHT
+            break;
+        // case 'ArrowRight':
+        //     userInput = GameInput.RIGHT
+        //     break;
+    
+        case 'KEYD':
+            userInput = GameInput.LEFT
+            break;
+        // case 'ArrowLeft':
+        //     userInput = GameInput.LEFT
+        //     break;
+    
+        // case 'ArrowUp':
+        //     userInput = GameInput.FORWARD
+        //     break;
+        case 'KeyW':
+            userInput = GameInput.FORWARD
+            break;
+
                     
         default:
             userInput = null
